06cb66a202b8e6cfc99b7c386bc81fd448cca0ef
felerticia <felerticia@gmail.com>
2023-08-23 09:43:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './App.css'
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState(0)
+  const [laps, setLaps] = useState([])
+  const [isRunning, setIsRunning] = useState(false)
 
-  const [time,setTime] = useState(0)
-  const [isRunning,setIsRunning] = useState(false)
-  const [laps,setLaps] = useState([])
+  const getMinutes = ms => ("0" + Math.floor((ms / 60 / 1000) % 60)).slice(-2)
+  const getSeconds = ms => ("0" + Math.floor((ms / 1000) % 60)).slice(-2)
+  const getMilliSeconds = ms => ("0" + (ms / 10) % 100).slice(-2)
 
-
-  const getMin = ms => ('0' + Math.floor((ms / 1000 / 60) % 60)).slice(-2)
-  const getSec = ms => ('0' + Math.floor((ms / 1000) % 60)).slice(-2)
-  const getHundredth = ms => ('0' + (ms / 10) % 100).slice(-2)
-
-
-  const formatTime = ms => `${getMin(ms)}:${getSec(ms)}.${getHundredth(ms)}`
+  const formatTime = ms => `${getMinutes(ms)}:${getSeconds(ms)}.${getMilliSeconds(ms)}`
 
   useEffect(() => {
-    let interval;
-    if(isRunning){
-      interval = setInterval(()=> {setTime(time => time + 10)},
-      10)
-    }
+    let interval = null;
 
+    if (isRunning) {
+      interval = setInterval(() => setTime(time => time + 10), 10);
+    } 
+    
     return () => clearInterval(interval);
-  },[isRunning])
+  }, [isRunning]);
 
   useEffect(() => {
-    if (time){
-      const rest = laps.slice(0,laps.length-1)
+    if (time) {
+      const rest = laps.slice(0,laps.length-1)    
       let last = laps[laps.length-1]
-      last = time - rest.reduce((acc,v) => acc+v , 0)
+      last = time - rest.reduce((acc,x) => acc+x, 0)
       setLaps([...rest,last])
     }
     else {
@@ -40,16 +37,20 @@ function App() {
   return (
     <div className="App">
       <div className="display">{formatTime(time)}</div>
+
       <div className="buttons">
         {!isRunning && !time && <button onClick={() => setIsRunning(true)}>Start</button>}
         {!isRunning && time > 0 && <button onClick={() => setIsRunning(true)}>Resume</button>}
         {isRunning && <button onClick={() => setIsRunning(false)}>Stop</button>}
-        {isRunning &&<button onClick={() => setLaps([...laps, 0])}>Lap</button>}
-        {!isRunning && time>0 && <button onClick={() => setTime(0)}>Reset</button>}
+        {isRunning && <button onClick={() => setLaps([...laps,0])}>Lap</button>}
+        {!isRunning && time > 0 && <button onClick={() => setTime(0)}>Reset</button>}
+      </div>
 
+      <div className="laps">
+        {laps.map((x,i) => <div key={i}>Lap {i+1}: {formatTime(x)}</div>)}
       </div>
     </div>
   );
-}
+};
 
 export default App;
